a814944058ca51329894ae346bd38944cf8eb8c9
Guillaume MANCIET <mudounet@free.fr>
2016-11-20 09:47:22 +0100

    Adding base transform script.

---
+++ transform.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use POSIX;
+use strict;
+use warnings;
+use Log::Log4perl qw(:easy);
+
+Log::Log4perl->easy_init($DEBUG);
+
+my $maxLesson = 112 ; # nombre maxi de lecons
+my $phaseMixte = 50; # Phase mixte
+my $phaseActive = 71; # phase active seule
+my $maxModule = ceil ($maxLesson/7);
+my $link_base = 'http://cours.toutapprendre.com/eja0a/';
+
+use constant GRAMMARY => 1;
+use constant GRAMMARY_AND_ACTIVE => 2;
+use constant LISTENING => 3;
+use constant LISTENING_AND_ACTIVE => 4;
+use constant ACTIVE => 5;
+use constant EXT => '.asp';
+use constant DIR_SOUNDS => 'eja0a/sons/';
+
+unlink 'wget_log.txt';
+for my $lesson(1..$maxLesson) {
+	my ($type, $linkExt) = getTypeLesson($lesson);
+	INFO "Processing lesson #".sprintf("%03d", $lesson);
+	
+}
+
+sub getTypeLesson {
+	my ($l) = @_;
+	my $ext = '.asp';
+	my $num = '';
+	my $m = ceil($l/7);
+	
+	my $link = '?l=' . $l . '&m='  .  $m; # . '&num=' . $num;
+	my $type;
+	
+	return (ACTIVE, $link) if($l >= $phaseActive);
+	if ($l >= $phaseMixte) {
+		return (GRAMMARY_AND_ACTIVE, $link) if($l == ($m*7) && $l !=70);
+		return (LISTENING_AND_ACTIVE, $link);
+	}
+	return (GRAMMARY, $link) if($l == ($m*7) && $l !=70);
+	return (LISTENING, $link);
+}
